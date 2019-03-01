eaa523b351a1cfc51b8c2080ec60cf372ab44a37
KathrynJaneway <KathrynJaneway@web.de>
2019-03-01 22:51:06 +0100

    first_Site: Layout of the headers, navbar and copyright set. Works in chrome and firefox.

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -7,34 +7,44 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="css/theme.css">
 </head>
-<body>
-<div class="container">
 
-    <div class="page-header">
+<div class="container-header container-fluid">
+
+    <header>
         @include('includes.header')
-    </div>
+    </header>
 
-    <div id="main" class="row">
+</div>
 
+<body>
+    <div class="container-sidebar container-fluid">
         <!-- sidebar content -->
         <div id="sidebar" class="col-md-12 text-center">
             @include('includes.navbar')
         </div>
 
-        <!-- main content -->
-        <div id="content" class="col-md-12">
-            @yield('content')
+    </div>
+
+    <div class="container-content container-fluid">
+
+        <div class="row">
+            <div class="body" class="col-md-12 text-center">
+                Hello World!
+            </div>
         </div>
 
     </div>
 
-    <div class="footer-copyright text-right py-3">
-        @include('includes.footer')
+    <div class="container-copyright container-fluid">
+
+        <div class="footer-copyright" class="col-md-12 text-center">
+            @include('includes.footer')
+        </div>
+
     </div>
 
 
-</div>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
